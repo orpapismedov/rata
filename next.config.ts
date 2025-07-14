5cf6d350d9831691c7216d84a5498a948adc065e
or papismedov <orpapismedov@gmail.com>
2025-07-14 16:43:21 +0300

    Add basePath configuration for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/rata',
+  assetPrefix: '/rata/',
   images: {
     unoptimized: true
   },
